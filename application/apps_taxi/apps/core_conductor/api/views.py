28d1ae53ca8a8ce7f78178a3e05c6d8fac997fba
murayama-takumi-1216 <murayama.takumi.20021216@gmail.com>
2025-11-20 16:50:21 +0100

    search limited to first name only fix

--- application/apps_taxi/apps/core_conductor/api/views.py
+++ application/apps_taxi/apps/core_conductor/api/views.py
@@ -1,5 +1,6 @@
 import logging
 
+from django.db.models import Q
 from rest_framework.viewsets import ModelViewSet
 from rest_framework_datatables.filters import DatatablesFilterBackend
 from rest_framework_datatables.renderers import DatatablesRenderer
@@ -23,9 +24,15 @@ class ListaConductorParaAlquilerViewSet(ProtectedAdministradorApiView, ModelView
     renderer_classes = [DatatablesRenderer]
 
     def filter_queryset(self, queryset):
-        search_nombre = self.request.query_params.get("search[value]", None)
+        search_term = self.request.query_params.get("search[value]", None)
         queryset = queryset.filter(estado=CONDUCTOR_ESTADO_DISPONIBLE)
         # estado__in=[CONDUCTOR_ESTADO_DISPONIBLE, CONDUCTOR_ESTADO_OCUPADO]
-        if search_nombre:
-            queryset = queryset.filter(nombre__istartswith=search_nombre)
-        return queryset.order_by("nombre")
+        if search_term:
+            queryset = queryset.filter(
+                Q(cod_conductor__istartswith=search_term)
+                | Q(nombre__istartswith=search_term)
+                | Q(apellido_paterno__istartswith=search_term)
+                | Q(apellido_materno__istartswith=search_term)
+                | Q(licencia__istartswith=search_term)
+            )
+        return queryset.order_by("nombre", "apellido_paterno")
